7b9ec5a6c8d64911a871e0679b1e2cfb28662762
pratapaditya07 <139495535+pratapaditya07@users.noreply.github.com>
2025-03-20 02:23:14 +0530

    added footer

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-/* gsap.from(".contact",{
+gsap.from(".contact",{
     duration: 2, 
     y: -100, 
     opacity: 0, 
     ease: "bounce",
     stagger: 0.5
-}) */
+})
 
 gsap.to("nav h1",{
     fontSize: "2.5vw",
@@ -18,3 +18,31 @@ gsap.to("nav h1",{
         pin : "nav"
     }
 })
+
+gsap.from("#video_intro", {
+    scale : 0,
+    scrollTrigger: {
+        trigger : "#video_intro",
+        scroll: "main",
+        start : "top 120%",
+        end : "top 50%",
+        scrub: 1
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
